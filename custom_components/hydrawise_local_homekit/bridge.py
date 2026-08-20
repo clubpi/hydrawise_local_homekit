b85f1bf53ae26d889ead85b50934bd325d5e0733
clubpi <clubpi@users.noreply.github.com>
2026-08-20 13:56:21 +0200

    Reset HomeKit accessory model 0.4.4

--- custom_components/hydrawise_local_homekit/bridge.py
+++ custom_components/hydrawise_local_homekit/bridge.py
@@ -357,7 +357,7 @@ class HydrawiseHomeKitSystemBridge:
 
         persist_file = str(
             Path(self.hass.config.path(".storage"))
-            / f"hydrawise_local_homekit_{self.entry.entry_id}.state"
+            / f"hydrawise_local_homekit_{self.entry.entry_id}_bridge_v2.state"
         )
         pin = self.entry.data[CONF_PIN].encode()
 
